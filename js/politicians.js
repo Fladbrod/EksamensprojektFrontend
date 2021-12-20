3f3b3baf20791cae3384fc5fdb523a9c32757b28
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2021-12-20 23:38:30 +0100

    Frontend

--- js/politicians.js
+++ js/politicians.js
@@ -1,5 +1,5 @@
 const politicianTbody = document.getElementById("politicians-tbody");
-const newPoliticianSelect = document.getElementById("create-politician-party")
+const newPoliticianSelect = document.getElementById("create-politician-party");
 
 getPoliticians()
 
@@ -24,11 +24,11 @@ function constructPoliticianTableRow(tableRow, politician) {
     const actionTd = document.createElement("td");
 
     const updatePoliticianButton = document.createElement("button");
-    updatePoliticianButton.className = "button1";
+    updatePoliticianButton.className = "edit-btn";
     const acceptUpdateButton = document.createElement("button");
-    acceptUpdateButton.className = "button1";
+    acceptUpdateButton.className = "accept-btn";
     const deletePoliticianButton = document.createElement("button");
-    deletePoliticianButton.className = "button1";
+    deletePoliticianButton.className = "delete-btn";
 
     partyTd.innerText = politician.party.partyName;
     politicianTd.innerText = politician.politicianName;
@@ -141,8 +141,65 @@ function createPolitician() {
     }).then(result => {
         politicianToCreate.id = result.id;
         createPoliticianTableRow(politicianToCreate);
+        document.getElementById("create-politician-name").value = "";
     });
 }
 
 document.getElementById("new-politician-button")
     .addEventListener("click", createPolitician);
+
+// Med hjælp fra https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_number
+function sortTable(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("center-table");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount ++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
